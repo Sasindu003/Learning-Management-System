c217ff233e8a08e39786f68b3f4fa7d1c39f549e
Sasindu Akalanka <137613265+Sasindu003@users.noreply.github.com>
2026-03-30 11:55:05 +0530

    Delete src directory

--- src/main/java/com/lms/lms/exception/UserDeletionException.java
+++
@@ -1,7 +0,0 @@
-package com.lms.lms.exception;
-
-public class UserDeletionException extends RuntimeException {
-    public UserDeletionException(String message) {
-        super(message);
-    }
-}
